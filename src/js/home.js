be01d0c92f946c68ce7f108f9eb3ca052e3500c7
Geraint White <geraint@android.net>
2014-05-15 21:30:58 +0100

    Made site responsive and added social buttons

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,5 @@
 var user = 'grit96';
 
-github_widget(user, document.getElementById('github'));
+github_widget(user, document.getElementById('github-widget'));
 
 twitter_tagline(user, document.getElementById('bio'));
